10af9dd031773b51534752180ce3300d2b316639
Milton12-Mateus <51714420+Milton12-Mateus@users.noreply.github.com>
2021-06-10 19:23:19 -0300

    Arquivos do vosk foram Adicionados

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-"""
-#Nosso Ficheiro Main(Principal)
-
-import speech_recognition as sr
-import pyaudio
-
-r = sr.Recognizer()
-
-while True:
-    with sr.Microphone() as source:
-        audio = r.listen(source) # Define Microfone como Fonte de áudio
-        print(r.recognize_google(audio, language='pt')) """
-        
-        
-from vosk import Model, kaldiRecognizer
+from vosk import KaldiRecognizer, Model
 import os
-
-if not os.path.exists("model"):
-	print("Please download the model from https://https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-	exit (1)
-
 import pyaudio
 
-model = Model(model)
+model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
-p = pyaudio.Pyaudio()
+p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
